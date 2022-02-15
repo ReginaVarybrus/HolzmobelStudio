7949d3120a061144d461c6272d95f89e9d59e257
ReginaVarybrus <varybrusr@gmail.com>
2022-02-15 14:53:08 +0200

    container intro done

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   new Splide(".splide").mount()
 })
+
+var splide = new Splide(".splide", {
+  type: "fade",
+  rewind: true,
+})
+
+splide.mount()
