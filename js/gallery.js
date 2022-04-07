bf129fda2600b156205a8de2fb53e5f7a9c2dc76
ReginaVarybrus <varybrusr@gmail.com>
2022-04-07 23:04:31 +0300

    slider done

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   new Splide(".splide").mount()
+// })
+
+// var splide = new Splide(".splide", {
+//   type: "fade",
+//   rewind: true,
+// })
+
+// splide.mount()
+
 document.addEventListener("DOMContentLoaded", function () {
-  new Splide(".splide").mount()
+  splide.mount()
 })
 
 var splide = new Splide(".splide", {
   type: "fade",
   rewind: true,
+  autoplay: true,
+  pauseOnHover: true,
 })
-
-splide.mount()
